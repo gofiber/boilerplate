567c693feeaca392eb83f9a5a216e763a8d1368d
Fenny <25108519+Fenny@users.noreply.github.com>
2020-05-02 16:40:30 +0200

    Update Boilerplate

--- routes/notfound.go
+++ routes/notfound.go
@@ -2,11 +2,7 @@ package routes
 
 import "github.com/gofiber/fiber"
 
-// NotFound route
+// NotFound sends a 404 "Not Found"
 func NotFound(c *fiber.Ctx) {
-	c.Status(404)
-	c.Render("404", fiber.Map{
-		"Title":   "404 Not Found - Fiber",
-		"Message": "Sorry, but the page you were looking for could not be found.",
-	})
+	c.SendStatus(fiber.StatusNotFound)
 }
