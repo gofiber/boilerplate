d545f743fa01b16ce591e7e7586bf50845dbfa31
René <rene@gofiber.io>
2025-08-24 14:26:05 +0200

    migrate to v3

--- app.go
+++ app.go
@@ -4,12 +4,14 @@ import (
 	"boilerplate/database"
 	"boilerplate/handlers"
 
+	"github.com/gofiber/fiber/v3/middleware/static"
+
 	"flag"
 	"log"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3/middleware/recover"
 )
 
 var (
